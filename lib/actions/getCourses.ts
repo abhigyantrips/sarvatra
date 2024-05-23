5a0dc1b06ffe7998fb4d92afc95f8add0471355a
Abhigyan Tripathi <abhigyantrips@gmail.com>
2024-05-23 10:24:38 +0530

    chore(actions): reduce actions for data tables

--- lib/actions/getCourses.ts
+++
@@ -1,9 +0,0 @@
-'use server';
-
-import { db } from '@/lib/db';
-
-export async function getCourses() {
-  const courses = await db.course.findMany();
-
-  return courses;
-}
--- lib/actions/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './getCourses';
-export * from './getStudents';
-export * from './getSubjects';
-export * from './getResults';
-export * from './getUsers';
